728b10c09b124eba9c6e03cbd495d8cd72576370
zion ubesie <zionubesie@gmail.com>
2025-05-31 05:56:44 +0100

    minor fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,6 +8,7 @@ import { useContext } from "react";
 import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { generateRandomId } from "../Components/RandomId";
 import DatabaseLandingAnimation from "../Components/LandingPageAnimation";
+import googleImage from '../assets/google.bc5e59cc.svg'
 export const Login = () => {
   const navigate = useNavigate()
   const { user, setIsOpen } = useContext(UserContext)
@@ -20,7 +21,7 @@ export const Login = () => {
     try {
       const result = await signInWithPopup(auth, Provider);
       const q = query(docRef, where('userId', '==', result.user.uid))
-      const docs=await getDocs(q)
+      const docs = await getDocs(q)
       if (docs.empty) {
         await addDoc(collection(db, 'user'), {
           userId: result.user?.uid,
@@ -44,16 +45,23 @@ export const Login = () => {
 
     <div className="h-[630px] text-black">
       {!user && <div className="flex min-h-screen  h-full w-full">
-       <DatabaseLandingAnimation/>
-        <div className="w-[70%] h-full p-6 bg-[#ffffff] shadow-md  ">
+        <DatabaseLandingAnimation />
+        <div className=" edit-login min-[600px]:w-[70%] h-full p-6 bg-[#ffffff] shadow-md  ">
           <h2 className="mb-6 text-3xl font-bold text-center ">
             Login
           </h2>
           <Form />
-          <button className="w-full pt-4" onClick={signInPopup}>Sign up with Google</button>
+          <button className="w-full py-2 border-1 hover:border-blue-500 
+          hover:text-blue-500 mt-8 border-gray-300 flex justify-center items-center
+           rounded-3xl gap-2 transition-colors " onClick={signInPopup}>
+            <img src={googleImage} alt="" />
+            Sign in with Google
+          </button>
           <p className="mt-20 text-sm text-center  ">
             Don’t have an account?
-            <button onClick={() => { navigate('/register') }} className="text-[#213ec0f1] hover:underline">
+            <button onClick={() => { navigate('/register') }}
+              className="text-[#213ec0f1] hover:underline 
+            ">
               Sign up
             </button>
           </p>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,7 +11,7 @@ export const Register = () => {
   return (
     <div className="relative">
       <DatabaseLandingAnimation/>
-        <div className="w-full max-w-sm p-6 bg-white absolute text-black rounded-lg shadow-md top-10 left-[35%]">
+        <div className="w-full max-w-sm p-6 bg-white absolute text-black rounded-lg shadow-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-[600px]:w-[80%]">
           <h2 className="mb-6 text-3xl font-bold text-center  ">
             Sign up
           </h2>
